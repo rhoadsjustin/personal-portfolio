90fd09f1ae7c353cc1ba0ef10f7f2d139f401e80
Justin Rhoads <rhoadsjustin@gmail.com>
2017-08-22 13:26:21 -0500

    fixed styling and added skills carousel

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 // sanity check
 $(document).ready(function(){
  $('.carousel.carousel-slider').carousel();
+ $('.carousel').carousel();
  $('.parallax').parallax();
  $('.tap-target').tapTarget('open');
  $('.tap-target').tapTarget('close');
